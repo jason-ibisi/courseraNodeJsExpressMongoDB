800446cfa829464e6d33c923412ad12f3a43a7b3
Jason IBISI <daveyjones@grr.la>
2018-12-16 15:29:03 +0100

    Express Router

--- node-express/.eslintrc.js
+++ node-express/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    rules: {
+        "linebreak-style": 0,
+        "eslint linebreak-style": [0, "error", "windows"],
+        "indent": ["error", 4],
+        "no-unused-vars": ["error", {"args": "none" , "vars": "none"}]
+    }
 };
+/*eslint linebreak-style: ["error", "windows"]*/
