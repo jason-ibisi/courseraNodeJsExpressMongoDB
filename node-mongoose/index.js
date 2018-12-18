b2e1a3b3e00a42ab2509b7315648838415ae72f7
Jason IBISI <daveyjones@grr.la>
2018-12-18 10:17:45 +0100

    Mongoose ODM Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,26 +8,24 @@ const connect = mongoose.connect(url,{useNewUrlParser: true});
 connect.then((db) => {
     console.log('Connected successfully to database.');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test description'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
